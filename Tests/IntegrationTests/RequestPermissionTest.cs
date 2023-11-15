0c70e8e4c3e69cade7bdb8eb5735082fb5f9f946
AlejoMillo00 <alejo.millo@outlook.com>
2023-11-15 18:36:50 -0300

    Added Tests

---
+++ Tests/IntegrationTests/RequestPermissionTest.cs
@@ -0,0 +1,205 @@
+﻿using Application.Models;
+using Application.UseCases.PermissionOperation;
+using System.Net;
+
+namespace Tests.IntegrationTests;
+
+public sealed class RequestPermissionTest
+{
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    public async Task Should_Request_Permission_OK(int permissionType)
+    {
+        var permissionUoW = MockPermissionUoW();
+        var elasticService = MockElasticService();
+        var topicService = MockTopicService();
+        var handler = new CreatePermissionHandler(
+            permissionUoW, elasticService, topicService);
+
+        var request = new CreatePermissionRequest
+        {
+            EmployeeForename = "Test",
+            EmployeeSurname = "Test",
+            PermissionType = permissionType,
+        };
+
+        var permissionsCountBeforeRequest = Permissions.Count;
+
+        var sr = await handler.Handle(request, CancellationToken.None);
+
+        Assert.NotNull(sr);
+        Assert.True(sr.Success);
+        Assert.True(Permissions.Count == permissionsCountBeforeRequest + 1);
+    }
+
+    [Theory]
+    [InlineData(9313)]
+    [InlineData(8373)]
+    public async Task Should_Failed_With_Invalid_PermissionType(int permissionType)
+    {
+        var permissionUoW = MockPermissionUoW();
+        var elasticService = MockElasticService();
+        var topicService = MockTopicService();
+        var handler = new CreatePermissionHandler(
+            permissionUoW, elasticService, topicService);
+
+        var request = new CreatePermissionRequest
+        {
+            EmployeeForename = "Test",
+            EmployeeSurname = "Test",
+            PermissionType = permissionType,
+        };
+
+        var permissionsCountBeforeRequest = Permissions.Count;
+
+        var sr = await handler.Handle(request, CancellationToken.None);
+
+        Assert.NotNull(sr);
+        Assert.Equal(HttpStatusCode.BadRequest, sr.StatusCode);
+        Assert.False(sr.Success);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    public async Task Should_Get_Permission_Ok(int id)
+    {
+        var mapper = MockMapper();
+        var permissionQueries = MockPermissionQueries();
+        var elasticService = MockElasticService();
+        var topicService = MockTopicService();
+        var handler = new GetPermissionHandler(
+            permissionQueries, mapper, elasticService, topicService);
+
+        var request = new GetPermissionRequest
+        {
+            Id = id,
+        };
+
+        var sr = await handler.Handle(request, CancellationToken.None);
+
+        Assert.NotNull(sr);
+        Assert.True(sr.Success);
+        Assert.NotNull(sr.Content);
+        Assert.IsType<PermissionDto>(sr.Content);
+    }
+
+    [Theory]
+    [InlineData(1, 2)]
+    [InlineData(2, 1)]
+    public async Task Should_Modify_Permission_OK(int id, int permissionType)
+    {
+        var permissionUoW = MockPermissionUoW();
+        var elasticService = MockElasticService();
+        var topicService = MockTopicService();
+        var handler = new UpdatePermissionHandler(
+            permissionUoW, elasticService, topicService);
+
+        var request = new UpdatePermissionRequest
+        {
+            Id = id,
+            PermissionType = permissionType,
+        };
+
+        var permissionTypeBeforeUpdate = Permissions.FirstOrDefault(x => x.Id == id).PermissionType;
+
+        var sr = await handler.Handle(request, CancellationToken.None);
+
+        var updatedPermission = Permissions.FirstOrDefault(x => x.Id == id);
+
+        Assert.NotNull(sr);
+        Assert.True(sr.Success);
+        Assert.NotEqual(permissionTypeBeforeUpdate, updatedPermission.PermissionType);
+        Assert.Equal(permissionType, updatedPermission.PermissionType);
+    }
+
+    [Theory]
+    [InlineData(1, 3)]
+    [InlineData(2, 3)]
+    public async Task Should_Modify_Permission_Fail_With_Invalid_PermissionType(int id, int permissionType)
+    {
+        var permissionUoW = MockPermissionUoW();
+        var elasticService = MockElasticService();
+        var topicService = MockTopicService();
+        var handler = new UpdatePermissionHandler(
+            permissionUoW, elasticService, topicService);
+
+        var request = new UpdatePermissionRequest
+        {
+            Id = id,
+            PermissionType = permissionType,
+        };
+
+        var sr = await handler.Handle(request, CancellationToken.None);
+
+        Assert.NotNull(sr);
+        Assert.False(sr.Success);
+        Assert.Equal(HttpStatusCode.BadRequest, sr.StatusCode);
+    }
+
+    [Theory]
+    [InlineData(99, 2)]
+    [InlineData(999, 1)]
+    public async Task Should_Modify_Permission_Fail_NotFound(int id, int permissionType)
+    {
+        var permissionUoW = MockPermissionUoW();
+        var elasticService = MockElasticService();
+        var topicService = MockTopicService();
+        var handler = new UpdatePermissionHandler(
+            permissionUoW, elasticService, topicService);
+
+        var request = new UpdatePermissionRequest
+        {
+            Id = id,
+            PermissionType = permissionType,
+        };
+
+        var sr = await handler.Handle(request, CancellationToken.None);
+
+        Assert.NotNull(sr);
+        Assert.False(sr.Success);
+        Assert.Equal(HttpStatusCode.NotFound, sr.StatusCode);
+    }
+
+    [Theory]
+    [InlineData(99)]
+    [InlineData(999)]
+    public async Task Should_Fail_Get_Permission_NotFound(int id)
+    {
+        var mapper = MockMapper();
+        var permissionQueries = MockPermissionQueries();
+        var elasticService = MockElasticService();
+        var topicService = MockTopicService();
+        var handler = new GetPermissionHandler(
+            permissionQueries, mapper, elasticService, topicService);
+
+        var request = new GetPermissionRequest
+        {
+            Id = id,
+        };
+
+        var sr = await handler.Handle(request, CancellationToken.None);
+
+        Assert.NotNull(sr);
+        Assert.Equal(HttpStatusCode.NotFound, sr.StatusCode);
+        Assert.False(sr.Success);
+        Assert.Null(sr.Content);
+    }
+
+    [Fact]
+    public async Task Should_List_Permissions_Ok()
+    {
+        var mapper = MockMapper();
+        var permissionQueries = MockPermissionQueries();
+        var topicService = MockTopicService();
+        var handler = new ListPermissionsHandler(
+            permissionQueries, mapper, topicService);
+
+        var sr = await handler.Handle(new(), CancellationToken.None);
+
+        Assert.NotNull(sr);
+        Assert.True(sr.Success);
+        Assert.Equal(Permissions.Count, sr.Content.Count);
+    }
+}
---
+++ Tests/MockData.cs
@@ -0,0 +1,195 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using Application.Models;
+using AutoMapper;
+using Domain.Entities;
+using Moq;
+using System.Net;
+
+namespace Tests;
+
+public static class MockData
+{
+    public static List<PermissionType> PermissionTypes = new List<PermissionType>
+    {
+        new PermissionType
+        {
+            Id = 1,
+            Description = "Test Permission Type",
+        },
+        new PermissionType
+        {
+            Id = 2,
+            Description = "Test Permission Type 2",
+        },
+    };
+
+    public static List<Permission> Permissions = new List<Permission>
+    {
+        new Permission
+        {
+            Id = 1,
+            EmployeeForename = "Test 1",
+            EmployeeSurname = "Test |",
+            PermissionType = 1,
+            PermissionDate = new DateTime(2023, 11, 15),
+        },
+        new Permission
+        {
+            Id = 2,
+            EmployeeForename = "Test 2",
+            EmployeeSurname = "Test 2",
+            PermissionType = 2,
+            PermissionDate = new DateTime(2023, 11, 15),
+        },
+    };
+
+    public static IPermissionUoW MockPermissionUoW()
+    {
+        var mockPermissionUoW = new Mock<IPermissionUoW>();
+        var mockPermissionTypeQueries = MockPermissionTypeQueries();
+        var mockPermissionQueries = MockPermissionQueries();
+        var mockPermissionCommands = MockPermissionCommands();
+
+        mockPermissionUoW.Setup(m => m.PermissionTypeQueries).Returns(mockPermissionTypeQueries);
+        mockPermissionUoW.Setup(m => m.PermissionQueries).Returns(mockPermissionQueries);
+        mockPermissionUoW.Setup(m => m.PermissionCommands).Returns(mockPermissionCommands);
+
+        return mockPermissionUoW.Object;
+    }
+
+    public static IPermissionTypeQueries MockPermissionTypeQueries()
+    {
+        var mockPermissionTypeQueries = new Mock<IPermissionTypeQueries>();
+
+        mockPermissionTypeQueries.Setup(m => m.GetAsync(It.IsAny<int>()))
+            .ReturnsAsync((int id) =>
+            {
+                var permissionType = PermissionTypes.FirstOrDefault(x => x.Id == id);
+                return new ServiceResponse<PermissionType>
+                {
+                    Content = permissionType,
+                };
+            });
+
+        return mockPermissionTypeQueries.Object;
+    }
+
+    public static IPermissionQueries MockPermissionQueries()
+    {
+        var mockPermissionQueries = new Mock<IPermissionQueries>();
+
+        mockPermissionQueries.Setup(m => m.GetAsync(It.IsAny<int>()))
+            .ReturnsAsync((int id) => {
+                var permission = Permissions.FirstOrDefault(x => x.Id == id);
+                return new ServiceResponse<Permission>
+                {
+                    Content = permission,
+                };
+            });
+
+        mockPermissionQueries.Setup(m => m.ListAsync())
+            .ReturnsAsync(() => {
+                return new ServiceResponse<List<Permission>>
+                {
+                    Content = Permissions,
+                };
+            });
+
+        return mockPermissionQueries.Object;
+    }
+
+    public static IPermissionCommands MockPermissionCommands()
+    {
+        var mockPermissionQueries = new Mock<IPermissionCommands>();
+
+        mockPermissionQueries.Setup(m => m.CreateAsync(It.IsAny<Permission>()))
+            .ReturnsAsync((Permission permission) => {
+                permission.Id = Permissions.Last()?.Id + 1 ?? 1;
+                Permissions.Add(permission);
+                return new ServiceResponse<Permission>
+                {
+                    Content = permission,
+                    StatusCode = HttpStatusCode.Created,
+                };
+            });
+
+        mockPermissionQueries.Setup(m => m.UpdateAsync(It.IsAny<Permission>()))
+            .ReturnsAsync((Permission permission) => {
+                var permissionIndex = Permissions.FindIndex(x => x.Id == permission.Id);
+                if(permissionIndex == -1)
+                    return new ServiceResponse
+                    {
+                        StatusCode = HttpStatusCode.NotFound,
+                    };
+
+                Permissions[permissionIndex] = permission;
+                return new ServiceResponse
+                {
+                    StatusCode = HttpStatusCode.OK,
+                };
+            });
+
+        return mockPermissionQueries.Object;
+    }
+
+    public static IElasticService MockElasticService()
+    {
+        var mockElasticService = new Mock<IElasticService>();
+
+        mockElasticService.Setup(m => m.IndexAsync(It.IsAny<string>(), It.IsAny<It.IsAnyType>()))
+            .ReturnsAsync(new ServiceResponse());
+
+        return mockElasticService.Object;
+    }
+
+    public static ITopicService MockTopicService()
+    {
+        var mockTopicService = new Mock<ITopicService>();
+
+        mockTopicService.Setup(m => m.ProduceMessageAsync(It.IsAny<It.IsAnyType>()))
+            .ReturnsAsync(new ServiceResponse());
+
+        return mockTopicService.Object;
+    }
+
+    public static IMapper MockMapper()
+    {
+        var mockMapper = new Mock<IMapper>();
+
+        mockMapper.Setup(m => m.Map<PermissionDto>(It.IsAny<Permission>()))
+            .Returns((Permission src) =>
+            {
+                return new PermissionDto
+                {
+                    Id = src.Id,
+                    EmployeeForename = src.EmployeeForename,
+                    EmployeeSurname = src.EmployeeSurname,
+                    PermissionDate = src.PermissionDate.ToShortDateString(),
+                    PermissionType = src.PermissionType,
+                };
+            });
+
+        mockMapper.Setup(m => m.Map<List<PermissionDto>>(It.IsAny<List<Permission>>()))
+            .Returns((List<Permission> src) =>
+            {
+                var response = new List<PermissionDto>();
+
+                for(int i = 0; i < src.Count; i++)
+                {
+                    response.Add(new PermissionDto
+                    {
+                        Id = src[i].Id,
+                        EmployeeForename = src[i].EmployeeForename,
+                        EmployeeSurname = src[i].EmployeeSurname,
+                        PermissionDate = src[i].PermissionDate.ToShortDateString(),
+                        PermissionType = src[i].PermissionType,
+                    });
+                }
+
+                return response;
+            });
+
+        return mockMapper.Object;
+    }
+}
