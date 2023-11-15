7fb5bc7ed0ca54d99bc8fc5472412a952367db57
AlejoMillo00 <alejo.millo@outlook.com>
2023-11-15 18:42:58 -0300

    Removed  not used configuration keys

--- Shared/Configuration/ElasticConfigurationOptions.cs
+++ Shared/Configuration/ElasticConfigurationOptions.cs
@@ -6,6 +6,4 @@ public class ElasticConfigurationOptions
 {
     public static string Url { get; set; }
     public static string DefaultIndex { get; set; }
-    public static string Username { get; set; }
-    public static string Password { get; set; }
 }
--- Shared/Configuration/KafkaConfigurationOptions.cs
+++ Shared/Configuration/KafkaConfigurationOptions.cs
@@ -4,5 +4,4 @@ public class KafkaConfigurationOptions
 {
     public static string Endpoint { get; set; }
     public static string Topic { get; set; }
-    public static string ClientId { get; set; }
 }
